8831f04357327ce9de24883c2ffe1d91204acc1d
hideyuki.suzuki <070303+5@gmail.com>
2021-02-28 14:14:34 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-// 認証
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login')->name('login.post');
-Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
 
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
-Route::get('/', function () {
-    return view('welcome');
+// 認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+//認証付きのルーティング
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
 });
